39be739bd5e77e44ce3592033cf2fd5fb81ba873
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-14 21:25:19 +0200

    config css

--- gout-local/src/components/HeaderPage.vue
+++ gout-local/src/components/HeaderPage.vue
@@ -4,6 +4,25 @@
 
 <template>
     <header>
-        
+        <div class="w-[1920px] h-[104px] relative">
+        <div class="w-[1920px] h-[104px] absolute left-[-1px] top-[-1px] opacity-75 bg-neutral-50"></div>
+        <img src="logo.png" class="w-[66px] h-[81px] absolute left-[41px] top-3 object-cover" />
+        <a href="#" class="w-[205px] absolute left-[158px] top-[33px] text-[28px] font-semibold text-left text-[#413b3b]">
+            Nos partenaires
+        </a>
+        <a href="#" class="w-40 absolute left-[403px] top-[33px] text-[28px] font-semibold text-left text-[#413b3b]">
+            Nos recettes
+        </a>
+        <a href="#" class="absolute left-[603px] top-[33px] text-[28px] font-semibold text-left text-[#413b3b]">
+            Nos conseils
+        </a>
+        <a href="#" class="w-[165px] absolute left-[803px] top-[33px] text-[28px] font-semibold text-left text-[#413b3b]">
+            Évènements
+        </a>
+        <img src="panier.png" class="w-[30px] h-[30px] absolute left-[1657px] top-9 object-cover" />
+        <a href="#" class="w-[145px] absolute left-[1738px] top-[33px] text-[28px] font-semibold text-left text-[#413b3b]">
+            Connexion
+        </a>
+        </div>
     </header>   
 </template>
