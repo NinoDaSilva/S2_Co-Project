cb72448db0fffb7892ed05acbb3b0c4b34cd7720
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-15 23:20:25 +0200

    création Hero

---
+++ gout-local/src/components/Hero.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+    title: string;
+    imgPath: string;
+    imgAlt: string;
+    imgSndPath: string;
+    imgSndAlt: string;
+}>();
+</script>
+
+<template>
+    <div>
+        <div>
+            <h1>{{ title }}</h1>
+            <img :src="imgSndPath" :alt="imgSndAlt" />
+        </div>
+        <img :src="imgPath" :alt="imgAlt" />
+    </div>
+</template>
