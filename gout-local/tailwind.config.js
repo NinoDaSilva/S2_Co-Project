b67effae57f3cebaa712900222d1ad5f77a033ff
Nino DA SILVA <nino.da_silva@edu.univ-fcomte.fr>
2023-05-16 15:28:11 +0200

    config colors

--- gout-local/tailwind.config.js
+++ gout-local/tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    "colors": {
+      blanc: "#fafafa",
+      brun: "#413b3b",
+      vert: "#6ac25e",
+      vertFonce: "#129418",
+      vertBleu: "#00b98f",
+      brunClair: "#fdf4ee",
+      noir: "#1c1514",
+      bleuClair: "#c9f2e4",
+      bleuTurquoise: "#00aeb6"
+    },
     fontFamily: {
       specific: ['Mochiy Pop One', 'sans-serif'],
       Subheading: ['Montserrat', 'sans-serif'],
