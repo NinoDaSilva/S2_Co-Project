4978fa2c8578c0a0ac3192b306acbf8806c00edb
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-16 22:01:03 +0200

    header intégré

--- gout-local/src/components/HeaderPage.vue
+++ gout-local/src/components/HeaderPage.vue
@@ -2,9 +2,9 @@
 import { RouterLink } from "vue-router";
 
 import { ref, computed, watch } from 'vue';
-const activeMenu = ref(false);
+const menuIsOpen = ref(false);
 function closeMenu() {
-	  activeMenu.value = false
+	  menuIsOpen.value = false
 	}
 
 //import { PanierIcon } from "@/components/icons/PanierIcon.vue";
@@ -12,7 +12,7 @@ function closeMenu() {
 </script>
 
 <template>
-    <header class="bg-neutral-50 fixed z-10 flex w-full translate-y-0 items-stretch justify-between px-6 py-2 transition-all duration-300 ease-in-out
+    <header class="bg-blanc bg-opacity-80 fixed z-10 flex w-full translate-y-0 items-stretch justify-between px-6 py-2 transition-all duration-300 ease-in-out
         lg:py-0">
         
         <div class="flex items-center">
@@ -21,59 +21,57 @@ function closeMenu() {
             </a>
         </div>
 
-        <div class="flex items-center gap-4">
-
             <button class="relative z-10 flex h-5 w-8 flex-col justify-between lg:hidden"
-                @click="activeMenu = !activeMenu">
-                <div class="ease h-[2px] w-full transform rounded-full bg-black transition duration-300"
-                    :class="{ 'translate-y-[9px] rotate-45 bg-white': activeMenu }">
+                @click="menuIsOpen = !menuIsOpen">
+                <div class="ease h-[2px] w-full transform rounded-full bg-noir transition duration-300"
+                    :class="{ 'translate-y-[9px] rotate-45 bg-white': menuIsOpen }">
                 </div>
-                <div class="ease h-[2px] w-full transform rounded-full bg-black transition duration-300"
-                    :class="{ 'bg-white opacity-0': activeMenu }">
+                <div class="ease h-[2px] w-full transform rounded-full bg-noir transition duration-300"
+                    :class="{ 'bg-white opacity-0': menuIsOpen }">
                 </div>
-                <div class="ease h-[2px] w-full transform rounded-full bg-black transition duration-300"
-                    :class="{ '-translate-y-[9px] -rotate-45 bg-white': activeMenu }">
+                <div class="ease h-[2px] w-full transform rounded-full bg-noir transition duration-300"
+                    :class="{ '-translate-y-[9px] -rotate-45 bg-white': menuIsOpen }">
                 </div>
             </button>
-
         
-            <nav class="invisible opacity-0 fixed inset-0 h-screen w-screen bg-indigo text-2xl text-white transition-all duration-300 ease-in-out
+            <nav class="invisible opacity-0 fixed inset-0 h-screen w-screen text-center transition-all duration-300 ease-in-out
                 lg:visible lg:relative lg:flex lg:h-auto lg:items-center lg:tracking-wide lg:opacity-100"
-                :class="{ '!visible opacity-100': activeMenu }">
-                <ul class="font-primary font-semibold text-center text-lg mt-[10vh] md:flex lg:m-0 lg:flex">
-                    <li class="md:flex"> 
+                :class="{ '!visible opacity-100': menuIsOpen }">
+                <ul class="font-primary font-semibold text-lg mt-[16vh] lg:m-0 lg:flex">
+                    <li> 
                         <RouterLink class="menu-link" to="/pages/produits" 
                             @click="closeMenu">Nos produits</RouterLink>
                     </li>
-                    <li class="md:flex">
+                    <li>
                         <RouterLink class="menu-link" to="/pages/recettes"
                             @click="closeMenu">Nos recettes</RouterLink>
                     </li>
-                    <li class="md:flex">
+                    <li>
                         <RouterLink class="menu-link" to="/pages/conseils"
                             @click="closeMenu">Nos conseils</RouterLink>
                     </li>
-                    <li class="md:flex">
+                    <li>
                         <RouterLink class="menu-link" to="/pages/events"
                             @click="closeMenu">Évènements</RouterLink>
                     </li>
-                    <li class="md:flex">
+                    <li>
                         <RouterLink class="menu-link" to="/pages/partenaires"
                             @click="closeMenu">Nos partenaires</RouterLink>
                     </li>
-                    <li class="md:flex">
-                        <RouterLink class="menu-link" to="/pages/panier"
-                            @click="closeMenu">
-                            <PanierIcon /></RouterLink>
-                    </li>
                 </ul>
-                <span class="text-center md:flex">
-                    <RouterLink class="menu-link" to="/pages/connexion"
-                        @click="closeMenu">Connexion</RouterLink>
-                </span>
+                
             </nav>
 
-        </div>
+
+            <div class="font-primary font-semibold text-lg invisible opacity-0 fixed inset-0 text-center transition-all duration-300 ease-in-out mt-[5vh]
+                lg:visible lg:relative lg:flex lg:h-auto lg:items-center lg:tracking-wide lg:opacity-100 lg:mt-[0vh] lg:justify-end"
+                :class="{ '!visible opacity-100': menuIsOpen }">
+                <RouterLink class="menu-link" to="/pages/panier"
+                        @click="closeMenu"><PanierIcon /></RouterLink>
+
+                <RouterLink class="menu-link" to="/pages/connexion"
+                    @click="closeMenu">Connexion</RouterLink>
+            </div>
 
     </header>   
 </template>
