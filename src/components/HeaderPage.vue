d42f054e7b6f85fcba00844aa29c1840a81eb448
Nino DA SILVA <nino.da_silva@edu.univ-fcomte.fr>
2023-05-21 22:12:30 +0200

    ajout fonction show/hide header

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -5,14 +5,28 @@ import PanierIcon from '@/components/icons/PanierIcon.vue'
 import GoutLocalLogo from '@/components/icons/GoutLocalLogo.vue'
 
 import { ref, computed, watch } from 'vue'
+import { useWindowScroll } from '@vueuse/core'
+
 const menuIsOpen = ref(false)
 function closeMenu() {
   menuIsOpen.value = false
 }
+
+const { y } = useWindowScroll()
+const underLimit = computed(() => y.value < 400)
+
+const dirTop = ref(true)
+watch(y, (y, oldY) => {
+  dirTop.value = y < oldY
+})
 </script>
 
 <template>
   <header
+    :class="{
+      '!-translate-y-full !bg-transparent': !dirTop,
+      '!bg-transparent lg:!bg-white': underLimit
+    }"
     class="bg-blanc bg-opacity-80 fixed z-20 flex w-full translate-y-0 items-center justify-between px-6 py-2 transition-all duration-300 ease-in-out text-sm xl:text-lg"
   >
     <div class="mr-8">
@@ -27,15 +41,18 @@ function closeMenu() {
     >
       <div
         class="ease h-[2px] w-full transform rounded-full bg-noir transition duration-300"
-        :class="{ 'translate-y-[9px] rotate-45 bg-blanc': menuIsOpen }"
+        :class="{ 'translate-y-[9px] rotate-45 bg-blanc': menuIsOpen,
+                '!bg-blanc': underLimit}"
       ></div>
       <div
         class="ease h-[2px] w-full transform rounded-full bg-noir transition duration-300"
-        :class="{ 'opacity-0': menuIsOpen }"
+        :class="{ 'opacity-0': menuIsOpen,
+                '!bg-blanc': underLimit }"
       ></div>
       <div
         class="ease h-[2px] w-full transform rounded-full bg-noir transition duration-300"
-        :class="{ '-translate-y-[9px] -rotate-45 bg-blanc': menuIsOpen }"
+        :class="{ '-translate-y-[9px] -rotate-45 bg-blanc': menuIsOpen,
+                '!bg-blanc': underLimit }"
       ></div>
     </button>
 
