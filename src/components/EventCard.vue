2bae0a60eac36eef437d8f428ae01fee4fe6a9e7
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-23 18:55:19 +0200

    modification

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-const props = defineProps<{
-  id: number
-  title: string
-  imgPath: string
-  imgAlt: string
-  description: string
-  date: string
-}>()
+import type { MaisonResponse } from '@/pocketbase-types';
+const props: MaisonResponse = defineProps<MaisonResponse>();
+
+import { pb } from '@/backend';
+const img0 = props.images[0];
+
+const urlImg0 = img0 && pb.getFileUrl(props, img0, { thumb: '400x400' });
 </script>
