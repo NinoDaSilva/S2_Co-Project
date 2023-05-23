dd32beef5a015532aed7b01766f7637d389efbcd
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-23 20:17:50 +0200

    création page about

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -13,14 +13,26 @@ import Hero from '@/components/Hero.vue'
     imgAlt="Image bannière page en savoir plus"
   >
   </Hero>
-</template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+  <div>
+    <h2>Nino DA SILVA</h2>
+    <p>Étudiant de 19 ans, je suis l’un des fondateurs du service Goût local et plus spécialement le développeur de ce site web.</p>
+    <p>Avec mes deux collègues, lors de la création de cette société, le but était simple pour nous : partager et mettre en avant le savoir faire des producteurs locaux avec leurs produits !</p>
+    <img src="/pu" alt="">
+  </div>
+
+  <div>
+    <h2>Lucas CHIPPAUX</h2>
+    <p>Étudiant de 18 ans, je suis l’un des fondateurs du service Goût local. Avec mes deux amis, lors de la création de cette société, le but était simple pour nous :</p>
+    <p>partager et mettre en avant nos producteurs locaux et leurs produits !</p>
+    <img src="" alt="">
+  </div>
+  <div>
+
+    <h2>Faris HALEPOVIC</h2>
+    <p>Étudiant de 20 ans, je suis l’un des fondateurs du service Goût local ou j’étais charger de faire la partie design du site avec Lucas CHIPPAUX.</p>
+    <p>Avec mes deux amis, lors de la création de cette société, le but était simple pour nous : partager et mettre en avant nos producteurs locaux et leurs produits !</p>
+    <img src="" alt="">
+  </div>
+
+</template>
