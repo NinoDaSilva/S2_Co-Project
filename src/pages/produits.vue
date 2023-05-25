f35f445b0e64824027440167234f3f7c6460e4b1
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-25 21:59:37 +0200

    mise en forme page produits

--- src/pages/produits.vue
+++ src/pages/produits.vue
@@ -15,13 +15,13 @@ useHead ({
   title: 'Nos produits'
 }) 
 //
-// const fruitsEte = await FruitsEte();
-// const fruitsAutomne = await FruitsAutomne();
-// const legumesEte = await LegumesEte();
-// const legumesAutomne = await LegumesAutomne();
-// const legumesPrintemps = await LegumesPrintemps();
-// const legumesHiver = await LegumesHiver();
-// const produitListe = await allProduits();
+const fruitsEte = await FruitsEte();
+const fruitsAutomne = await FruitsAutomne();
+const legumesEte = await LegumesEte();
+const legumesAutomne = await LegumesAutomne();
+const legumesPrintemps = await LegumesPrintemps();
+const legumesHiver = await LegumesHiver();
+//const produitListe = await allProduits();
 </script>
 
 <template>
@@ -32,21 +32,41 @@ useHead ({
   >
   </Hero>
 
-  <section>
+  <section class="mb-10 mt-10 sm:ml-10 lg:ml-0 duration-300">
     <div>
-      <h2>Les fruits de saison</h2>
-      <div class="underligne w-[8vh] sm:w-[12vh] lg:w-[15vh]"></div>
+      <h2 class="">Les fruits de saison</h2>
+      <div class="underligne bg-vertBleu w-5/6"></div>
     </div>
-    <p>En <strong>été</strong> vous trouverez...</p>
-    <CardProduit v-for="produit in produitListe" :v-key="produit.id" v-bind="{ ...produit }" />    
+    <p class="mt-4 mb-4 lg:mt-12">En <strong>été</strong> vous trouverez...</p>
+    <div class="grid grid-cols-3 gap-5 p-2 md:gap-20 sm:p-10">
+      <CardProduit class="border-bleuClair sm:border-8 max-w-[300px]" v-for="produit in fruitsEte" :v-key="produit.id" v-bind="{ ...produit }" />    
+    </div>
+    <p class="mt-4 mb-4 lg:mt-20">En <strong>automne</strong> vous trouverez...</p>
+    <div class="grid grid-cols-3 gap-5 p-2 md:gap-20 sm:p-10">
+      <CardProduit class="border-bleuClair sm:border-8 max-w-[300px]" v-for="produit in fruitsAutomne" :v-key="produit.id" v-bind="{ ...produit }"/>
+    </div>
   </section>
 
-  <section>
+  <section class="mb-10 mt-10 sm:ml-10 lg:ml-0 duration-300">
     <div>
       <h2>Les légumes de saison</h2>
-      <div class="underligne w-[8vh] sm:w-[12vh] lg:w-[15vh]"></div>
+      <div class="underligne bg-vertBleu w-5/6"></div>
     </div>
-    <p>En <strong>automne</strong> vous trouverez...</p>
-      <CardProduit v-for="produit in produitListe" :v-key="produit.id" v-bind="{ ...produit }"/>
+    <p class="mt-4 mb-4 lg:mt-12">Au <strong>printemps</strong> vous trouverez...</p>
+    <div class="grid grid-cols-3 gap-5 p-2 md:gap-20 sm:p-10">
+      <CardProduit class="border-bleuClair sm:border-8 max-w-[300px]" v-for="produit in legumesPrintemps" :v-key="produit.id" v-bind="{ ...produit }"/>
+    </div>
+    <p class="mt-4 mb-4 lg:mt-20">En <strong>été</strong> vous trouverez...</p>
+    <div class="grid grid-cols-3 gap-5 p-2 md:gap-20 sm:p-10">
+      <CardProduit class="border-bleuClair sm:border-8 max-w-[300px]" v-for="produit in legumesEte" :v-key="produit.id" v-bind="{ ...produit }"/>
+    </div>
+    <p class="mt-4 mb-4 lg:mt-20">En <strong>automne</strong> vous trouverez...</p>
+    <div class="grid grid-cols-3 gap-5 p-2 md:gap-20 sm:p-10">
+      <CardProduit class="border-bleuClair sm:border-8 max-w-[300px]" v-for="produit in legumesAutomne" :v-key="produit.id" v-bind="{ ...produit }"/>
+    </div>
+    <p class="mt-4 mb-4 lg:mt-20">En <strong>hiver</strong> vous trouverez...</p>
+    <div class="grid grid-cols-3 gap-5 p-2 md:gap-20 sm:p-10">
+      <CardProduit class="border-bleuClair sm:border-8 max-w-[300px]" v-for="produit in legumesHiver" :v-key="produit.id" v-bind="{ ...produit }"/>
+    </div>
   </section>
 </template>
