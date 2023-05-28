68394de28896137375e7629c3cfb55785046d3c8
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-28 02:22:20 +0200

    ajout script

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import PocketBase from 'pocketbase'
+
 import HeaderPage from '@/components/HeaderPage.vue'
 import FooterPage from '@/components/FooterPage.vue'
+// PocketBase vps connection
+var pocketbase_ip=''
+if(import.meta.env.MODE === 'production')
+  pocketbase_ip='http://193.168.145.101:80'
+else
+  pocketbase_ip='http://127.0.0.1:8090'
+
+const pb = new PocketBase(pocketbase_ip)
+//
 </script>
 
 <template>
--- src/pages/inscription.vue
+++ src/pages/inscription.vue
@@ -1,7 +1,63 @@
-<script setup lang="ts">
-
+<script setup>
+import btn from '@/components/btn.vue';
+import Hero from '@/components/Hero.vue'
+import ConnexionIcon from '@/components/icons/ConnexionIcon.vue';
+//
+import {useHead} from '@unhead/vue'
+useHead ({
+  title: 'Inscription'
+}) 
+//
 </script>
 
 <template>
-    
+    <Hero
+    title="Inscription"
+    imgPath="/img/hero/connexion.webp"
+    imgAlt="Image bannière page connexion"
+    >
+    </Hero>
+
+    <form class="max-w-md md:max-w-xl mx-auto px-8 pt-6 mb-12 mt-4 sm:mt-8">
+        <ConnexionIcon class="w-1/3 md:w-2/5 mx-auto mb-5 sm:mb-10"/>
+        <div class="py-2 sm:py-3">
+            <label>Nom</label>
+            <input class="form-champ border-bleuTurquoise" type="text" id="name" placeholder="Entrez votre nom">
+        </div>
+        <div class="py-2 sm:py-3">
+            <label>Prénom</label>
+            <input class="form-champ border-bleuTurquoise" type="text" id="first-name" placeholder="Entrez votre prénom">
+        </div>
+        <div class="py-2 sm:py-3">
+            <label>Mail</label>
+            <input class="form-champ border-bleuTurquoise" type="email" id="login" placeholder="Entrez votre adresse mail">
+        </div>
+        <div class="py-2 sm:py-3">
+            <label>Mot de passe</label>
+            <input class="form-champ border-bleuTurquoise" type="password" id="passwd" placeholder="Entrez votre mot de passe">
+        </div>
+        <div class="py-2 sm:py-3">
+            <label>Confirmation du mot de passe</label>
+            <input class="form-champ border-bleuTurquoise" type="password" id="passwd" placeholder="Confirmer votre mot de passe">
+        </div>
+        <div class="text-center mt-4 sm:mt-10">
+            <button v-on:click="register()"><btn text="S'inscrire"/></button>
+        </div>
+    </form>
 </template>
+
+<script>
+export default {
+  methods: {
+    async register() {
+      await pb.collection('users').create({
+        email: document.getElementById("login").value,
+        password: document.getElementById("passwd").value,
+        passwordConfirm: document.getElementById("passwd").value,
+        name: document.getElementById("name").value,
+        first: document.getElementById("first-name").value,
+      });
+    }
+  }
+}
+</script>
