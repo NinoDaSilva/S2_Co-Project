1698051416b17ee484ed1db663129a738d8aed27
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-28 22:19:25 +0200

    update

---
+++ src/pages/espace.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Hero from '@/components/Hero.vue'
+//
+import {useHead} from '@unhead/vue'
+useHead ({
+  title: 'Inscription'
+}) 
+//
+</script>
+
+<template>
+    <Hero
+    title="Votre espace personnel"
+    imgPath="/img/hero/connexion.webp"
+    imgAlt="Image bannière page connexion"
+    >
+    </Hero>
+</template>
