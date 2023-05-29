ec3e1bd48138959a6796787b04db5eed0154e5f3
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-29 15:13:53 +0200

    modification mise en forme

--- src/components/GoogleMaps.vue
+++ src/components/GoogleMaps.vue
@@ -15,8 +15,8 @@ export default defineComponent({
 </script>
 
 <template>
-    <div>
-        <GoogleMap api-key="AIzaSyBkkUWcQ46TA630_IUv2_sJlC5CZblRMkk" style="width: 100%; height: 500px" :center="center" :zoom="12">
+    <div class="overflow-hidden w-full h-[400px] rounded-xl border-4 border-vert mb-5 shadow-md sm:h-[550px]">
+        <GoogleMap api-key="AIzaSyBkkUWcQ46TA630_IUv2_sJlC5CZblRMkk" style="width: 100%; height: 100%" :center="center" :zoom="12">
             <Marker :options="markerOptions" />
         </GoogleMap>
     </div>
