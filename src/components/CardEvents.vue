56d95d9c0f54d7d9afb047a79e4f62401d9bd198
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-29 17:08:00 +0200

    ajout newsletter - page event

--- src/components/CardEvents.vue
+++ src/components/CardEvents.vue
@@ -9,12 +9,12 @@ const urlImg0 = img0 && pb.getFileUrl(props, img0, { thumb: '400x400' });
 </script>
 
 <template>
-    <div class="max-sm:w-[300px] sm:flex sm:my-20 my-3 overflow-hidden rounded-3xl border-2 border-vert shadow-sm mx-auto max-w-[1500px] duration-300">
+    <div class="max-sm:w-[280px] sm:flex sm:my-20 my-3 overflow-hidden rounded-3xl border-2 border-vert shadow-sm mx-auto max-w-[1500px] duration-300">
         <img class="w-full h-40 sm:h-full sm:w-60 lg:w-[400px] lg:h-[400px] " :src="urlImg0" alt="photo evenement">
         <div class="text-center sm:text-start p-4 sm:pb-0 lg:px-8 xl:mt-12 xl:px-14">
             <h2 class="font-Subheading mb-2 font-medium text-vertFonce sm:text-sm lg:text-lg xl:text-xl">{{ title }}</h2>
             <span class="font-secondary text-5xs lg:text-2xs ml-4">{{ date }}</span>
-            <p class="mt-4 text-start sm:mt-1 sm:leading-5 sm:text-2xs md:ml-4 lg:text-base lg:leading-7 lg:mt-5">{{ description }}</p>
+            <p class="text-5xs mt-4 text-start sm:mt-1 sm:leading-5 sm:text-2xs md:ml-4 lg:text-base lg:leading-7 lg:mt-5">{{ description }}</p>
         </div>
     </div>
 </template>
--- src/pages/events.vue
+++ src/pages/events.vue
@@ -2,6 +2,7 @@
 import Hero from '@/components/Hero.vue'
 import CardEvents from '@/components/CardEvents.vue'
 import { allEvents } from '@/backend';
+import btn from '@/components/btn.vue';
 //
 import {useHead} from '@unhead/vue'
 useHead ({
@@ -18,5 +19,42 @@ const eventsListe = await allEvents();
     imgAlt="Image bannière page evenements"
   >
   </Hero>
-  <CardEvents v-for="events of eventsListe" :v-key="events.id" v-bind="{ ...events}"/>
+  <section class="max-sm:grid max-sm:grid-cols-1">
+    <CardEvents v-for="events of eventsListe" :v-key="events.id" v-bind="{ ...events}"/>
+  </section>
+
+  <section class="mt-10 mb-10 sm:mb-20">
+    <div class="underligne bg-bleuTurquoise mx-auto w-full sm:w-5/6"></div>
+    <h2 class="max-sm:text-2xs text-center mt-8">Suivez les infos avec notre Newsletter</h2>
+    <form class="md:max-w-2xl mx-auto px-5 sm:p-14 sm:px-20 mt-8 sm:border-2 border-bleuTurquoise rounded-3xl">
+      <div class="mb-4">
+        <label class="form-label--home" for="last-name">
+          Nom
+        </label>
+        <input class="form-champ border-bleuTurquoise"
+          id="last-name" type="text" placeholder="Entrez votre nom">
+      </div>
+      <div class="mb-4">
+        <label class="form-label--home" for="first-name">
+          Prénom
+        </label>
+        <input class="form-champ border-bleuTurquoise"
+          id="first-name" type="text" placeholder="Entrez votre prénom">
+      </div>
+      <div class="mb-4">
+        <label class="form-label--home" for="email">
+          Mail
+        </label>
+        <input class="form-champ border-bleuTurquoise"
+          id="email" type="email" placeholder="Entrez votre adresse mail">
+      </div>
+      <div class="mt-5 items-center">
+        <input class="mr-3 sm:mr-5" id="confidential" type="checkbox">
+        <label for="confidential">J'accepte <RouterLink to="/mentions" class="link"><strong>la politique de confidentialité</strong></RouterLink></label>
+      </div>
+      <div class="flex items-center justify-center mt-6 sm:mt-10">
+        <btn text="Envoyez" class="bg-bleuTurquoise lg:px-12"/>
+      </div>
+    </form>
+  </section>
 </template>
