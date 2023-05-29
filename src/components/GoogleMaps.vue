8f274a15ae3c0294dcdd3a0c1de9f4b6519000e6
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-05-29 21:37:31 +0200

    ajout marker sur la map

--- src/components/GoogleMaps.vue
+++ src/components/GoogleMaps.vue
@@ -1,23 +1,29 @@
 <script>
 import { defineComponent } from 'vue'
-import { GoogleMap, Marker } from 'vue3-google-map'
+import { GoogleMap, Marker, MarkerCluster } from 'vue3-google-map'
 
 export default defineComponent({
-  components: { GoogleMap, Marker },
+  components: { GoogleMap, Marker, MarkerCluster },
   setup() {
-    const center = { lat: 40.689247, lng: -74.044502 };
-    const markerOptions = { position: center, title: "LADY LIBERTY" };
+    const center = { lat: 47.42463267366512, lng: 6.905441485714746 } // Gaec Voireuchon
 
-    return { center, markerOptions };
+    const locations = [
+      { lat: 47.42463267366513, lng: 6.905441485714748 }, // Gaec Voireuchon
+      { lat: 47.74102471685159, lng: 6.6167847865846605 }, //L'éden de Célestine
+      { lat: 47.4374973867614, lng: 6.6838067893560975 }, //Georgel Philippe
+      { lat: 47.4744446959658, lng: 6.433263521718228 }, // Ferme d'Uzelle
+    ]
+    return { center, locations }
   },
-});
-
+})
 </script>
 
 <template>
     <div class="overflow-hidden w-full h-[400px] rounded-xl border-4 border-vert mb-5 shadow-md sm:h-[550px]">
-        <GoogleMap api-key="AIzaSyBkkUWcQ46TA630_IUv2_sJlC5CZblRMkk" style="width: 100%; height: 100%" :center="center" :zoom="12">
-            <Marker :options="markerOptions" />
+        <GoogleMap api-key="AIzaSyCleJlTtLhLa-ZYXCJvtetFl4O2PpA42sI" style="width: 100%; height: 100%" :center="center" :zoom="9">
+          <MarkerCluster>
+            <Marker v-for="(location, i) in locations" :options="{ position: location }" :key="i" />
+          </MarkerCluster>
         </GoogleMap>
     </div>
 </template>
