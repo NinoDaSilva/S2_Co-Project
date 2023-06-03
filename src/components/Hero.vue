11b25c94d5b711d6f50e5d4a37f2a2032fa0b433
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-06-03 17:36:10 +0200

    correction title size

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -12,7 +12,7 @@ defineProps<{
   <div class="relative z-0 sm:-mx-[100px]">
     <img class="max-sm:h-[170px] w-full" :src="imgPath" :alt="imgAlt" />
     <div class="absolute left-[16%] right-[14%] bottom-7 sm:bottom-[30%]">
-      <h1 class="title-head">{{ title }}</h1>
+      <h1 class="title-head max-w-[1130px] mx-auto">{{ title }}</h1>
       <slot name="chapo"></slot>
     </div>
     <div class="underligne w-full rounded-none"></div>
