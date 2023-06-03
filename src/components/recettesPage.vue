30e7ff6b0d453c81b6a60edc9573e04628f5734a
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-06-03 18:35:15 +0200

    mise en place page recettes

--- src/components/recettesPage.vue
+++ src/components/recettesPage.vue
@@ -1,7 +1,51 @@
 <script setup lang="ts">
+import { pb } from '@/backend';
+import type { RecettesResponse } from '@/pocketbase-types';
+import Hero from './Hero.vue';
+import peopleIcon from '@/components/icons/peopleIcon.vue';
+
+const props: RecettesResponse = defineProps<RecettesResponse>();
+const img = props.image_hero;
+
+const urlImg0 = img && pb.getFileUrl(props, img, { thumb: '400x400' });
 
 </script>
 
 <template>
+    <Hero
+        :title="titre"
+        :imgPath="urlImg0"
+        imgAlt="Image bannière page recettes"
+    >
+    <template #chapo>
+        <p class="title-head--recette">Recette facile : {{ temps }} min</p>
+    </template>
+    </Hero>
 
+    <div class="max-sm:mx-5 sm:ml-10 max-md:text-center mb-10 mt-8 sm:mb-20 sm:mt-20">
+        <h2 class="max-md:mb-5 sm:-ml-10">Découvrez notre recettes de <span class="lowercase">{{ titre }}</span> facile à réaliser !</h2>
+        <div class="max-md:hidden mb-8 underligne bg-vertBleu w-full max-w-[80vh]"></div>
+    </div>
+
+    <div>
+        <span>Pour {{ nbr_personnes }} personnes : </span>
+        <peopleIcon class="inline-block mb-1 ml-1 w-5 h-5" v-for="personnes in nbr_personnes" />
+    </div>
+
+    <div>
+        <div>
+            <!-- etapes -->
+        </div>
+        <!-- img -->
+    </div>
+
+    <section class="mt-10">
+        <div class="ml-10 mb-4">
+            <h3 class="-ml-5">Ingrédients</h3>
+            <div class="underligne bg-vertBleu max-sm:w-[8vh] max-w-[12vh]"></div>
+      </div>
+      <div>
+        <!-- img -->
+      </div>
+    </section>
 </template>
--- src/pages/recettes/index.vue
+++ src/pages/recettes/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { allRecettes } from '@/backend';
+import { recetteSalee, recetteSucree } from '@/backend';
 import Hero from '@/components/Hero.vue'
 import btn from '@/components/btn.vue';
 import recetteCard from '@/components/CardRecette.vue';
@@ -9,7 +9,8 @@ useHead ({
   title: 'Nos recettes'
 }) 
 //
-const recettesListe = await allRecettes();
+const recettesSucreeListe = await recetteSucree();
+const recettesSaleeListe = await recetteSalee();
 </script>
 
 <template>
@@ -27,27 +28,27 @@ const recettesListe = await allRecettes();
 
   <div>
     <h3>Recettes salées</h3>
-    <ul>
-      <li v-for="unrecette of recettesListe" :v-key="unrecette.id">
-        <RouterLink :to="{ name: 'recettes-id', params: { id: unrecette.id }}">
-            <recetteCard v-bind="{ ...unrecette}" />
+    <ul class="grid grid-cols-2 lg:max-w-[1800px] mt-5 md:mt-10 mx-auto md:grid-cols-3 md:px-10 duration-300 pb-16">
+      <li v-for="unerecette of recettesSaleeListe" :v-key="unerecette.id">
+        <RouterLink :to="{ name: 'recettes-id', params: { id: unerecette.id }}">
+            <recetteCard v-bind="{ ...unerecette}" />
         </RouterLink>
         </li>
     </ul>
   </div>
 
-  <div>
+  <div class="lg:mt-20">
     <h3>Recettes sucrées</h3>
-    <ul>
-      <li v-for="unrecette of recettesListe" :v-key="unrecette.id">
-        <RouterLink :to="{ name: 'recettes-id', params: { id: unrecette.id }}">
-            <recetteCard v-bind="{ ...unrecette}" />
+    <ul class="grid grid-cols-2 lg:max-w-[1800px] mt-5 md:mt-10 mx-auto md:grid-cols-3 md:px-10 duration-300 pb-5 lg:pb-16">
+      <li v-for="unerecette of recettesSucreeListe" :v-key="unerecette.id">
+        <RouterLink :to="{ name: 'recettes-id', params: { id: unerecette.id }}">
+            <recetteCard v-bind="{ ...unerecette}" />
         </RouterLink>
         </li>
     </ul>
   </div>
 
-  <div class="max-sm:mx-5 sm:ml-10 text-center mb-10 mt-8 sm:mb-20 sm:mt-20 xl:flex">
+  <div class="max-sm:mx-5 sm:ml-10 text-center mb-16 mt-8 sm:mb-32 sm:mt-20 xl:flex">
     <div class="xl:mr-32">
       <h2 class="max-md:mb-5 md:text-start sm:-ml-10">Soutenir nos partenaires et leurs produits</h2>
       <div class="max-md:hidden mb-8 underligne bg-vertBleu sm:w-full max-w-[55vh]"></div>
