fa94fdf9247087b4a28b2ccf9774932eea1b9ce5
Ninnops <nino.da_silva@edu.univ-fcomte.fr>
2023-06-04 22:20:56 +0200

    ajout style et en savoir plus

--- src/components/recettesPage.vue
+++ src/components/recettesPage.vue
@@ -4,6 +4,7 @@ import type { RecettesResponse } from '@/pocketbase-types';
 import Hero from './Hero.vue';
 import peopleIcon from '@/components/icons/peopleIcon.vue';
 import RecetteIcon from './icons/RecetteIcon.vue';
+import btn from '@/components/btn.vue';
 
 const props: RecettesResponse = defineProps<RecettesResponse>();
 const img = props.image_hero;
@@ -28,48 +29,48 @@ const urlImg0 = img && pb.getFileUrl(props, img, { thumb: '400x400' });
         <div class="max-md:hidden mb-8 underligne bg-vertBleu w-full max-w-[80vh]"></div>
     </div>
 
-    <div>
+    <div class="max-sm:mx-5">
         <span>Pour {{ nbr_personnes }} personnes : </span>
         <peopleIcon class="inline-block mb-1 ml-1 w-4 sm:w-5 sm:h-5 " v-for="personnes in nbr_personnes" />
     </div>  
 
-    <div class="flex">
+    <div class="max-sm:mx-5 flex max-w-[1820px]">
         <!-- <ul v-for="(etape, index) in etapes" :v-key="index">
             <li v-bind="{ ...etape }">Étape {{ index + 1 }} : {{ etape }}</li>
         </ul> -->
-        <ul>
-            <li v-if="etape_1">
-                <span>Étape 1 : {{ etape_1 }}</span>
+        <ul class="mt-5">
+            <li v-if="etape_1" class="mt-3">
+                <span><strong>Étape 1 :</strong> {{ etape_1 }}</span>
             </li>
-            <li v-if="etape_2">
-                <span>Étape 2 : {{ etape_2 }}</span>
+            <li v-if="etape_2" class="mt-3">
+                <span><strong>Étape 2 :</strong> {{ etape_2 }}</span>
             </li>
-            <li v-if="etape_3">
-                <span>Étape 3 : {{ etape_3 }}</span>
+            <li v-if="etape_3" class="mt-3">
+                <span><strong>Étape 3 :</strong> {{ etape_3 }}</span>
             </li>
-            <li v-if="etape_4">
-                <span>Étape 4 : {{ etape_4 }}</span>
+            <li v-if="etape_4" class="mt-3">
+                <span><strong>Étape 4 :</strong> {{ etape_4 }}</span>
             </li>
-            <li v-if="etape_5">
-                <span>Étape 5 : {{ etape_5 }}</span>
+            <li v-if="etape_5" class="mt-3">
+                <span><strong>Étape 5 :</strong> {{ etape_5 }}</span>
             </li>
-            <li v-if="etape_6">
-                <span>Étape 6 : {{ etape_6 }}</span>
+            <li v-if="etape_6" class="mt-3">
+                <span><strong>Étape 6 :</strong> {{ etape_6 }}</span>
             </li>
-            <li v-if="etape_7">
-                <span>Étape 7 : {{ etape_7 }}</span>
+            <li v-if="etape_7" class="mt-3">
+                <span><strong>Étape 7 :</strong> {{ etape_7 }}</span>
             </li>
-            <li v-if="etape_8">
-                <span>Étape 8 : {{ etape_8 }}</span>
+            <li v-if="etape_8" class="mt-3">
+                <span><strong>Étape 8 :</strong> {{ etape_8 }}</span>
             </li>
-            <li v-if="etape_9">
-                <span>Étape 9 : {{ etape_9 }}</span>
+            <li v-if="etape_9" class="mt-3">
+                <span><strong>Étape 9 :</strong> {{ etape_9 }}</span>
             </li>
-            <li v-if="etape_10">
-                <span>Étape 10 : {{ etape_10 }}</span>
+            <li v-if="etape_10" class="mt-3">
+                <span><strong>Étape 10 :</strong> {{ etape_10 }}</span>
             </li>
         </ul>
-        <RecetteIcon class="w-1/6" />
+        <RecetteIcon class="max-md:hidden w-1/6 md:w-[300px] mb-60" />
     </div>
 
     <section class="mt-10">
@@ -81,4 +82,14 @@ const urlImg0 = img && pb.getFileUrl(props, img, { thumb: '400x400' });
         <!-- img -->
       </div>
     </section>
+
+    <div class="max-sm:mx-5 sm:ml-10 text-center mb-16 mt-8 sm:mb-32 sm:mt-20 xl:flex">
+    <div class="xl:mr-32">
+      <h2 class="max-md:mb-5 md:text-start sm:-ml-10">Découvrez d'autres recettes facile à réaliser !</h2>
+      <div class="max-md:hidden mb-8 underligne bg-vertBleu sm:w-full max-w-[58vh]"></div>
+    </div>
+    <RouterLink to="/partenaires">
+      <btn text="En savoir plus"/>
+    </RouterLink>
+  </div>
 </template>
